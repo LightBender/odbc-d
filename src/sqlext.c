9732ea67ba075bfbc6dc4b01e2756b63c964d49e
Adam Wilson <adamw@ellipticbit.com>
2023-09-16 02:18:42 -0700

    Initial Commit. First pass of ODBC DI generation from the Microsoft ODBC C headers.

---
+++ src/sqlext.c
@@ -0,0 +1,22 @@
+#define wchar_t unsigned short
+#define VOID void
+typedef wchar_t WCHAR;
+typedef WCHAR *LPWSTR;
+typedef int BOOL;
+typedef char CHAR;
+typedef unsigned char BYTE;
+typedef unsigned long DWORD;
+typedef unsigned short WORD;
+typedef unsigned char BYTE;
+typedef unsigned long DWORD;
+typedef unsigned short WORD;
+
+typedef unsigned long long UINT64;
+typedef long long INT64;
+
+typedef SQLGUID GUID;
+
+#include <sal.h>
+#include <no_sal2.h>
+
+#include "..\spec\Windows\inc\sqlext.h"
